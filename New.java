60207325e92da54e7312be4c84c2fd3949a0ce3d
tusharkhatri434 <tusharkhatri8193@gmail.com>
2024-06-24 06:43:46 +0530

    inital commit

---
+++ New.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class New{
+    public static void main(String[] args){
+        System.out.println("hello wd");
+
+        Scanner input = new Scanner(System.in);
+        int q = input.nextInt();
+        System.out.println(q);
+            }
+}
